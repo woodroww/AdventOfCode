11219401f2db305697eee39ff40d8689403f34da
woodroww <example528@gmail.com>
2022-12-01 11:05:31 -0500

    parsing day and year from current dir

--- 2022/support/src/main.rs
+++ 2022/support/src/main.rs
@@ -29,12 +29,29 @@ fn main() {
     let dir = std::env::current_dir().expect("couldn't get current dir");
     let file = dir.file_name().unwrap().to_str().unwrap();
     
+    println!("you are in {}", dir.as_path().to_str().expect("can't string"));
     if !file.starts_with("day") {
         panic!("you are not in a day folder");
     }
 
-    println!("you are in {}", dir.as_path().to_str().expect("can't string"));
-    println!("you are on day {}", file.strip_prefix("day").unwrap());
+    let day = file.strip_prefix("day")
+        .expect("error removing day from string")
+        .parse::<u8>()
+        .expect("error parsing u8 from day string");
+
+    println!("you are on day {}", day);
+
+    let year = dir.parent()
+        .expect("file has no parent")
+        .file_name()
+        .expect("couldn't get file_name")
+        .to_str()
+        .expect("couldn't convert osstring to string")
+        .parse::<u16>()
+        .expect("couldn't parse string into a year");
+
+    println!("you are in year {}", year);
+
 }
 
 
