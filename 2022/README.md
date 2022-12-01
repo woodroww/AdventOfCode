365ddbfe16a2cd32208eed4b34cfe69da5b8f084
woodroww <example528@gmail.com>
2022-12-01 11:41:06 -0500

    can download input if in a day? folder

---
+++ 2022/README.md
@@ -0,0 +1,1 @@
+set AOC_SESSION environment variable to the cookie from the aoc website
--- 2022/support/src/main.rs
+++ 2022/support/src/main.rs
@@ -1,16 +1,13 @@
-use dotenv;
+use std::io::Write;
 
 fn get_input(year: u16, day: u16) -> String {
     let url = format!("https://adventofcode.com/{year}/day/{day}/input");
     let client = reqwest::blocking::Client::builder().build().unwrap();
-    let session_cookie = dotenv::var("session").unwrap();
+    let session_cookie = std::env::var("AOC_SESSION").unwrap();
 
     let response = client
         .get(url)
-        .header(
-            "Cookie",
-            format!("session={}", session_cookie),
-        )
+        .header("Cookie", format!("session={}", session_cookie))
         .send()
         .unwrap();
 
@@ -23,25 +20,28 @@ fn get_input(year: u16, day: u16) -> String {
 
 // /Users/matt/prog/adventOfCode/2022/support/target/debug/support
 
-fn main() {
-    dotenv::dotenv().ok();
-
+fn parse_year_day() -> (u16, u16) {
     let dir = std::env::current_dir().expect("couldn't get current dir");
     let file = dir.file_name().unwrap().to_str().unwrap();
-    
-    println!("you are in {}", dir.as_path().to_str().expect("can't string"));
+
+    println!(
+        "you are in {}",
+        dir.as_path().to_str().expect("can't string")
+    );
     if !file.starts_with("day") {
         panic!("you are not in a day folder");
     }
 
-    let day = file.strip_prefix("day")
+    let day = file
+        .strip_prefix("day")
         .expect("error removing day from string")
-        .parse::<u8>()
+        .parse::<u16>()
         .expect("error parsing u8 from day string");
 
     println!("you are on day {}", day);
 
-    let year = dir.parent()
+    let year = dir
+        .parent()
         .expect("file has no parent")
         .file_name()
         .expect("couldn't get file_name")
@@ -51,7 +51,23 @@ fn main() {
         .expect("couldn't parse string into a year");
 
     println!("you are in year {}", year);
-
+    (year, day)
 }
 
+fn main() {
+    let (year, day) = parse_year_day();
+    let input = get_input(year, day);
 
+    let mut out_path = std::env::current_dir().expect("couldn't get current dir");
+    out_path.push("input.txt");
+    let mut file = std::fs::File::options()
+        .create(true)
+        .truncate(true)
+        .write(true)
+        .open(&out_path)
+        .expect(&format!(
+            "file {} couldn't be created",
+            out_path.as_path().to_str().expect("can't stringify path")
+        ));
+    file.write_all(input.as_bytes()).expect("file didn't write");
+}
