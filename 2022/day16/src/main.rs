0aa6baf3c24ab2a5f28647e6d3e2d30707fbbbac
woodroww <example528@gmail.com>
2022-12-16 13:59:05 -0500

    day16 can parse input

--- 2022/day16/src/main.rs
+++ 2022/day16/src/main.rs
@@ -1,5 +1,59 @@
+use std::collections::HashMap;
+
+struct Valve {
+    name: String,
+    flow_rate: usize,
+}
+
+struct ValveLayout {
+    valve_map: HashMap<String, Vec<String>>,
+    flows: HashMap<String, usize>,
+}
+
+impl std::fmt::Display for ValveLayout {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:#?}", self.valve_map)?;
+        write!(f, "{:#?}", self.flows)
+    }
+}
+
+fn parse_input(input: &str) -> ValveLayout {
+
+    let mut valve_map: HashMap<String, Vec<String>> = HashMap::new();
+    let mut flows = HashMap::new();
+
+    for line in input.lines() {
+        let parts = line.split(" ").collect::<Vec<&str>>();
+        //println!("{}", parts[1]);
+        let valve = parts[1];
+        let flow = parts[4].strip_suffix(';').unwrap();
+        let (_, flow) = flow.rsplit_once('=').unwrap();
+        let flow = flow.parse::<usize>().unwrap();
+        let already_existed = flows.insert(valve.to_string(), flow);
+        assert!(already_existed == None);
+        //println!("{}", flow);
+        let valve_entry = valve_map.entry(valve.to_string()).or_insert(vec![]);
+        for part in &parts[9..] {
+            let leads_to = match part.strip_suffix(',') {
+                Some(s) => s,
+                None => part,
+            };
+            //println!("{}", leads_to);
+            valve_entry.push(leads_to.to_string());
+        }
+        //println!();
+    }
+
+    ValveLayout {
+        valve_map,
+        flows,
+    }
+}
 
 fn part_1(input: &str) -> String {
+    let what = parse_input(input);
+
+    println!("{:#}", what);
     "".to_string()
 }
 
